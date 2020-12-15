555f304ec850c8d0ddf56005bf0e4f097aa1895a
GitCatalinC <74587861+GitCatalinC@users.noreply.github.com>
2020-12-15 20:23:10 +0200

    8

--- Monochrome/js/app.js
+++ Monochrome/js/app.js
@@ -1,1 +1,10 @@
-alert('hello world');
+let lookbookPage = document.querySelector('.monochrome-lookbook');
+
+if (lookbookPage !== null) {
+  const formClass = 'form-lookbook';
+  const form = document.querySelector(`.${formClass}`);
+  let pristine = null;
+  if (form !== null) {
+    pristine = new pristine(form);
+  }
+}
